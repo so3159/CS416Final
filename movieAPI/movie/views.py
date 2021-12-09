abe351dde9d8ce8a6bde781693bce2df9f0a9498
JoThurst <jothurst@gmail.com>
2021-12-08 22:46:59 -0500

    Messages/List Functionality/Frontend Pages

--- movieAPI/movie/views.py
+++ movieAPI/movie/views.py
@@ -8,6 +8,8 @@ from accounts.models import Profile
 from movie.models import Movie, List
 
 from django.db.models import Count
+from django.contrib import messages
+
 
 import requests
 # Create your views here.
@@ -83,6 +85,8 @@ def addListName(request):
             form.instance.profile = profile
             form.save()
             
+            messages.success(request, 'Successfully Added List!')
+
             context ={
                 'lists' :lists,
                 'profile':profile,
@@ -108,14 +112,15 @@ def addMoviesToList(request):
         movies = request.POST.getlist('movies')
         list_name = request.POST.get('list_name')
         print("Hello Am Here")
-        print(List.objects.filter(profile=profile).count)
+        print(List.objects.filter(profile=profile).count())
         if List.objects.filter(profile=profile).count() == 0:
             list = List.objects.create(profile = profile, name = 'My List 1')
         else:    
-            list = List.objects.filter(profile=profile).filter(name=list_name)
+            list = List.objects.filter(profile=profile).get(name=list_name)
+            
 
-
-        #print(list)
+        print("this is the list")
+        print(list)
         
         searchTerm = request.POST.get('search_term')
         #print(f'{movies} + {list} + {searchTerm}')
@@ -161,18 +166,110 @@ def addMoviesToList(request):
                 poster = data['image']
                 
                 #print(f'{title}  {year}  {runtime}  {rated} {plot} {awards} {directors} {stars} {genres} {released} {poster} ')
-
                 movie = Movie.objects.create(imbd_id =imbd_id, title = title, year = year, Runtime= runtime, Plot = plot, Awards = awards, Directors = directors, Stars = stars, Genres = genres, Rating = rated, Trailer = trailer, Released = released, Poster = poster)
             else:     
                 print("movie Already excists")
                 movie = Movie.objects.get(imbd_id=x)
-
+                
             list.movies.add(movie)
         # user = request.user
         # profile = Profile.objects.get(user=user)
+        messages.success(request, 'Successfully Added Movies!')
+
         return redirect(search)
     
     #return redirect(search)
     #profile.watched.add(movie)
     #print("added" + f'movie')
     
+    
+def editListView(request):
+    #get list name, get user, get profile, render edit list page
+    user = request.user.id
+    profile = Profile.objects.get(user = user)
+    if request.method == 'POST':
+        list_name = request.POST.get('list_name')
+        list= List.objects.get(name=list_name)
+        
+        movies = list.movies.all()
+        context = {
+            
+            'list_name':list_name,
+            'list':movies,
+            'user':user,
+            'profile':profile,
+        }
+
+        return render(request,'editList.html',context )
+        
+#get User/Profile, Get ListName and New List name from form
+#get List, Update List name to new name, save
+def updateListName(request):
+    user=request.user.id
+    profile=Profile.objects.get(user=user)
+    if request.method == 'POST':
+        list_name = request.POST.get('list_name')
+        new_name = request.POST.get('new_list_name')
+        list = List.objects.get(name = list_name)
+        list.name = new_name
+        list.save()
+        movies = list.movies.all()
+        context={
+            'list_name':new_name,
+            'list': movies,
+            'user':user,
+            'profile':profile,
+        }        
+        messages.success(request, 'Successfully Updated List Name From:'+ list_name + ' to: ' + new_name +' !')
+        return render(request,'editList.html',context)
+        
+    
+    
+
+#Delete One Movie From List At a Time And Re Render The List
+#Get User/Profile, Get List and ID from POST form
+# Remove Movie from list, render page.
+def deleteMovieFromList(request):
+    user = request.user.id
+    profile = Profile.objects.get(user = user)
+    
+    if request.method == 'POST':
+        #get list name, get movie id from form
+        list_name = request.POST.get('list_name')
+        movie_id= request.POST.get('imbd_id')
+        #get list object, get movie object 
+        list=List.objects.get(name=list_name)
+        movie = Movie.objects.get(imbd_id=movie_id)
+        list.movies.remove(movie)
+        movies = list.movies.all()
+        
+        context={
+            'list_name':list_name,
+            'list':movies,
+            'user':user,
+            'profile':profile,
+            
+        }
+        messages.warning(request, 'Successfully Deleted ' + movie.title +' from list ' + list_name + '!')
+
+        return render(request, 'editList.html',context)
+        
+        
+        
+#Delete Entire List and Redirect to Profile
+#Get User/Profile from Request, get Get List name from form
+#Get List object and Delete From Database
+def deleteList(request):
+    user = request.user.id
+    profile = Profile.objects.get(user = user)
+    
+    if request.method == 'POST':
+        list_name = request.POST.get('list_name')
+        list=List.objects.get(name=list_name)
+        list.delete()
+        messages.warning(request, 'Successfully Deleted '  + list_name + '  !')
+
+        return redirect('view_profile')
+    
+    return redirect('view_profile')
+        
